9ed9e199836825fa03ad0926b6f077289266d9cd
Brad Svercl <bradsvercl@gmail.com>
2021-04-15 19:11:24 -0500

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -8,8 +8,6 @@ const fs = std.fs;
 const mem = std.mem;
 const process = std.process;
 
-const Allocator = mem.Allocator;
-
 export fn handlerRoutine(dwCtrlType: winapi.DWORD) winapi.BOOL {
     return switch (dwCtrlType) {
         winapi.CTRL_C_EVENT => winapi.TRUE,
@@ -21,6 +19,7 @@ export fn handlerRoutine(dwCtrlType: winapi.DWORD) winapi.BOOL {
     };
 }
 
+// removeSuffix removes the suffix from the slice
 fn removeSuffix(comptime T: type, slice: []const T, suffix: []const T) []const T {
     if (mem.endsWith(u8, slice, suffix)) {
         return slice[0 .. slice.len - suffix.len];
@@ -29,35 +28,35 @@ fn removeSuffix(comptime T: type, slice: []const T, suffix: []const T) []const T
     }
 }
 
+// pathWithExtension returns the path with the specified extension
 fn pathWithExtension(allocator: *mem.Allocator, path: []const u8, extension: []const u8) ![]const u8 {
     const path_extension = fs.path.extension(path);
     const path_no_extension = removeSuffix(u8, path, path_extension);
     return fmt.allocPrint(allocator, "{}.{}", .{ path_no_extension, extension });
 }
 
-fn trimSpaces(comptime T: type, slice: []const T) []const T {
-    return mem.trim(T, slice, &ascii.spaces);
+// trimSpaces removes spaces from the beginning and end of a string
+fn trimSpaces(slice: []const u8) []const u8 {
+    return mem.trim(u8, slice, &ascii.spaces);
 }
 
 pub fn main() anyerror!void {
-    const gpa = heap.c_allocator;
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const ally = &arena.allocator;
 
     // Collect arguments
-    const args = try process.argsAlloc(gpa);
-    defer process.argsFree(gpa, args);
+    const args = try process.argsAlloc(ally);
 
     // Shim filename
-    var program_path = try fs.selfExePathAlloc(gpa);
-    defer gpa.free(program_path);
-    var shim_path = pathWithExtension(gpa, program_path, "shim") catch {
+    var program_path = try fs.selfExePathAlloc(ally);
+    var shim_path = pathWithExtension(ally, program_path, "shim") catch {
         std.log.crit("Cannot make out shim path.", .{});
         return;
     };
-    defer gpa.free(shim_path);
 
     // Place to store the shim file contents
-    var cfg = std.BufMap.init(gpa);
-    defer cfg.deinit();
+    var cfg = std.BufMap.init(ally);
 
     // Open shim file for reading
     var shim_file = fs.openFileAbsolute(shim_path, .{}) catch {
@@ -68,20 +67,19 @@ pub fn main() anyerror!void {
 
     // Go through the shim file and collect key-value pairs
     var reader = shim_file.reader();
-    var line_buf: [1024 * 2]u8 = undefined;
+    var line_buf: [1024]u8 = undefined;
 
     while (try reader.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
         // Find index of equals, if it doesn't exist we just skip this line
         const equals_index = mem.indexOfScalar(u8, line, '=') orelse continue;
-        const key = trimSpaces(u8, line[0..equals_index]);
-        const value = trimSpaces(u8, line[equals_index + 1 .. line.len]);
+        const key = trimSpaces(line[0..equals_index]);
+        const value = trimSpaces(line[equals_index + 1 .. line.len]);
         // Put in the map
         try cfg.set(key, value);
     }
 
     // Arguments sent to the child
-    var cmd_args = std.ArrayList([]const u8).init(gpa);
-    defer cmd_args.deinit();
+    var cmd_args = std.ArrayList([]const u8).init(ally);
 
     // Add the program name from the path
     if (cfg.get("path")) |cfg_path| {
@@ -110,7 +108,6 @@ pub fn main() anyerror!void {
     }
 
     // Spawn child process
-    var child = try std.ChildProcess.init(cmd_args.items, gpa);
-    defer child.deinit();
+    var child = try std.ChildProcess.init(cmd_args.items, ally);
     _ = try child.spawnAndWait();
 }
