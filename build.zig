b4699ec10e8a7fee0b860d6dd45d88613208b2b1
Brad Svercl <bradsvercl@gmail.com>
2023-04-12 17:24:59 -0500

    update build.zig
    https://github.com/ziglang/zig/tree/406706fe6b5e969028a7c70247ebd1cb2b93102d

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
         .optimize = optimize,
     });
     exe.linkLibC();
-    exe.install();
+    b.installArtifact(exe);
 
     exe.single_threaded = true;
 
@@ -19,7 +19,7 @@ pub fn build(b: *Builder) void {
         exe.strip = true;
     }
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -28,12 +28,12 @@ pub fn build(b: *Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest(.{
+    const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-
+    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
